06c76850182b0419c66fe8589df34bc8cb8c24b6
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2021-07-25 22:56:17 +0700

    justify the navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,6 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('root') }}" :active="request()->routeIs('root')">
+                        {{ __('Home') }}
+                    </x-jet-nav-link>
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
@@ -138,11 +141,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+			<x-jet-responsive-nav-link href="{{ route('root') }}" :active="request()->routeIs('root')">
+                {{ __('Home') }}
+            </x-jet-responsive-nav-link>
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
-            </x-jet-responsive-nav-link>
-            <x-jet-responsive-nav-link href="{{ route('menu') }}" :active="request()->routeIs('menu')">
-                {{ __('Menu') }}
             </x-jet-responsive-nav-link>
         </div>
 
